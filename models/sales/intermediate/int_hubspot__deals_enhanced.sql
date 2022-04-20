b9e59e755c3db518bc46d152f6713431b2c68f88
Lucas Miura <101188873+lucasmiura22@users.noreply.github.com>
2022-04-20 01:13:09 -0300

    feat: remove owner model dependency

--- models/sales/intermediate/int_hubspot__deals_enhanced.sql
+++ models/sales/intermediate/int_hubspot__deals_enhanced.sql
@@ -15,28 +15,19 @@ with deals as (
     select *
     from {{ var('deal_pipeline_stage') }}
 
-), owners as (
-
-    select *
-    from {{ var('owner') }}
-
 ), deal_fields_joined as (
 
     select 
         deals.*,
         pipelines.pipeline_label,
         pipelines.is_active as is_pipeline_active,
-        pipeline_stages.pipeline_stage_label,
-        owners.email_address as owner_email_address,
-        owners.full_name as owner_full_name
+        pipeline_stages.pipeline_stage_label
 
     from deals    
     left join pipelines 
         on deals.deal_pipeline_id = pipelines.deal_pipeline_id
     left join pipeline_stages 
         on deals.deal_pipeline_stage_id = pipeline_stages.deal_pipeline_stage_id
-    left join owners 
-        on deals.owner_id = owners.owner_id
 )
 
 select *
